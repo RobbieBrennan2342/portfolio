62785bbc11d8388859f62f2f806f1ed1f8a88fa1
robbiebrennan2342 <robertb2342@gmail.com>
2022-01-15 15:27:11 +0000

    New portfolio website

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>To be filled in later</SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More!
+      </Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
